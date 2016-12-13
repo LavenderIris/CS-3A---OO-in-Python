dc2ea30295b94dffbb44b302b805f6fa6e7a420f
Finder <pfinder@acbc32d525f9.ant.amazon.com>
2016-12-12 23:00:46 -0800

    Unit test for lib.py and tictactoe.py.  Includes checking while ingame, gameover states and when player 1 or player 2 wins.

--- finalunittest.py
+++ finalunittest.py
@@ -39,6 +39,44 @@ class Test_libpy(unittest.TestCase):
         ans = True
         self.assertEqual(board.game_over, ans)
 
+    def test_check_player_1_wins(self):
+        """
+        Unit test to check when player 1 wins.
+        :return:
+        """
+        pygame.init()
+        board = Board(grid_size=3, box_size=100, border=50, line_width=10)
+        surface_size = board.surface.get_height()
+
+        # sets the board for gameover state
+        board.process_click(surface_size/2, surface_size/2)
+        board.process_click(surface_size/2, surface_size * 0.7)
+        board.process_click(surface_size * 0.7, surface_size/2)
+        board.process_click(surface_size/3, surface_size * 0.7)
+        board.process_click(surface_size * 0.3, surface_size/2)
+        ans = 1
+        self.assertEqual(board.check_for_winner(), ans)
+
+    def test_check_player_2_wins(self):
+        """
+        Unit test to check when player 2 wins
+        :return:
+        """
+        pygame.init()
+        board = Board(grid_size=3, box_size=100, border=50, line_width=10)
+        surface_size = board.surface.get_height()
+
+        # sets the board for gameover state
+        board.process_click(surface_size * 0.3, surface_size * 0.5)
+        board.process_click(surface_size * 0.7, surface_size * 0.3)
+        board.process_click(surface_size * 0.5, surface_size * 0.5)
+        board.process_click(surface_size * 0.7, surface_size * 0.5)
+        board.process_click(surface_size * 0.3, surface_size * 0.7)
+        board.process_click(surface_size * 0.7, surface_size * 0.7)
+        ans = 2
+        self.assertEqual(board.check_for_winner(), ans)
+
+
 if __name__ == "__main__":
     unittest.main()
 
